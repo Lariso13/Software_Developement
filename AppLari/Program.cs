f1914076861a29ee98ee67a456a609de303a78f1
Larison <larison.sepulveda01@gmail.com>
2021-09-22 21:43:37 -0400

    C# update

---
+++ AppLari/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lineUpo = "Esta es mi primera linea de codigo en C#";
+            Console.WriteLine(lineUpo);
+        }
+    }
+}
